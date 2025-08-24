d3d9a1b32e1697148e6d316b90cdc9c3805ece15
Alejandro175 <alejandroq175@gmail.com>
2025-08-24 17:59:14 +0200

    Inital commit

--- app/core/config.py
+++ app/core/config.py
@@ -4,13 +4,13 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     env: str = os.getenv("ENV", "unit-test")
     jwt_algorithm: str
-    jwt_public_key: str  # contiene direttamente la chiave, non un path
+    jwt_public_key: str 
     postgres_url: str
     rabbitmq_username: str
     rabbitmq_password: str
     rabbitmq_url: str
 
     class Config:
-        env_file = None  # nessun file .env, solo ENV
+        env_file = None
 
 settings = Settings()
